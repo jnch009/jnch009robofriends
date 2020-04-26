6abfe5310713c0fae94ab4e71373ebb5df0dda22
jnch009 <jngcheng@sfu.ca>
2020-04-25 22:29:50 -0700

    moving logic back into the return

--- src/CardList.js
+++ src/CardList.js
@@ -2,11 +2,18 @@ import React, { Fragment } from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  const robotArray = robots.map((robot) => (
-    <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />
-  ));
-
-  return <Fragment>{robotArray}</Fragment>;
+  return (
+    <Fragment>
+      {robots.map((robot) => (
+        <Card
+          key={robot.id}
+          id={robot.id}
+          name={robot.name}
+          email={robot.email}
+        />
+      ))}
+    </Fragment>
+  );
 };
 
 export default CardList;
