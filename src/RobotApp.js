1e55339ab9c450a51518aeb9a3c08b34061cb534
jnch009 <jngcheng@sfu.ca>
2020-04-26 20:10:55 -0700

    fetching from test API with loading indicator

--- src/RobotApp.js
+++ src/RobotApp.js
@@ -1,16 +1,27 @@
 import React, { Component, Fragment } from "react";
 import Filter from "./Filter";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import "./RobotApp.css";
 
+//smart component
 class RobotApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      robots: robots,
+      robots: [],
       search: "",
     };
+  }
+
+  componentDidMount() {
+    //convert to json then update the state
+    //fetch makes a request to a server, available on browsers
+    //part of the window object
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   handleInput = (input) => {
@@ -23,6 +34,15 @@ class RobotApp extends Component {
     const robotsFiltered = this.state.robots.filter((robot) =>
       robot.name.toLowerCase().includes(this.state.search)
     );
+
+    if (this.state.robots.length === 0) {
+      return (
+        <div className="flex justify-center items-center vh-75">
+          <h1>Loading</h1>
+        </div>
+      );
+    }
+
     return (
       <Fragment>
         <div className="tc flex flex-column items-center">
