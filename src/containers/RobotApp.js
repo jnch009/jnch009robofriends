e974229d4eb00cfa8685cd42212cc1648fcf0743
jnch009 <jngcheng@sfu.ca>
2020-05-19 21:24:14 -0700

    provider and store

--- src/containers/RobotApp.js
+++ src/containers/RobotApp.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import CardList from "../components/CardList";
-import ErrorBoundry from "../components/ErrorBoundry";
-import Filter from "../components/Filter";
-import Scroll from "../components/Scroll";
+import CardList from '../components/CardList';
+import ErrorBoundry from '../components/ErrorBoundry';
+import Filter from '../components/Filter';
+import Scroll from '../components/Scroll';
 
-import "./RobotApp.css";
+import './RobotApp.css';
 
 //smart component
 class RobotApp extends Component {
@@ -13,7 +13,7 @@ class RobotApp extends Component {
     super(props);
     this.state = {
       robots: [],
-      search: "",
+      search: '',
     };
   }
 
@@ -21,14 +21,15 @@ class RobotApp extends Component {
     //convert to json then update the state
     //fetch makes a request to a server, available on browsers
     //part of the window object
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
+    console.log(this.props.store.getState());
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
         this.setState({ robots: users });
       });
   }
 
-  handleInput = (input) => {
+  handleInput = input => {
     this.setState({
       search: input.target.value.toLowerCase(),
     });
@@ -36,17 +37,17 @@ class RobotApp extends Component {
 
   render() {
     const { robots, search } = this.state;
-    const robotsFiltered = robots.filter((robot) =>
-      robot.name.toLowerCase().includes(search)
+    const robotsFiltered = robots.filter(robot =>
+      robot.name.toLowerCase().includes(search),
     );
 
     return !robots.length ? (
-      <div className="flex justify-center items-center vh-75">
+      <div className='flex justify-center items-center vh-75'>
         <h1>Loading</h1>
       </div>
     ) : (
-      <div className="tc flex flex-column items-center">
-        <h1 className="f1-l f2-m f3">ROBOFRIENDS</h1>
+      <div className='tc flex flex-column items-center'>
+        <h1 className='f1-l f2-m f3'>ROBOFRIENDS</h1>
         <Filter handleInput={this.handleInput} />
         <Scroll>
           <ErrorBoundry>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import RobotApp from './containers/RobotApp';
+import { searchRobots } from './reducers';
 
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import "tachyons";
-import RobotApp from "./containers/RobotApp";
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-    <RobotApp />
+    <Provider store={store}>
+      <RobotApp />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
